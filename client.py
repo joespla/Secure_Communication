504c67a1a4a1dab92a84d0ef35787bf20da07fbc
Jorge Espinosa <joespla2000@gmail.com>
2018-04-21 14:49:49 -0500

    Server.py and Client.py added

---
+++ client.py
@@ -0,0 +1,9 @@
+# This is the message sender
+
+
+def algo():
+    print("Hola")
+
+
+if __name__ == '__main__':
+    algo()
---
+++ server.py
@@ -0,0 +1,23 @@
+# This is the message receiver
+
+import os
+from socket import *
+
+host = ""
+port = 13000
+buf = 1024
+address = (host, port)
+UDPSock = socket(AF_INET, SOCK_DGRAM)
+UDPSock.bind(address)
+print("Receiving messages. Please wait...")
+while True:
+    (data, address) = UDPSock.recvfrom(buf)
+    print("Received message: " + data)
+    if data == "exit":
+        break
+UDPSock.close()
+os._exit(0)
+
+
+
+
