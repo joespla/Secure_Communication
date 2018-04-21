36fecc652c940cd6d19f8c60b63dc4e02b8f4c15
Jorge Espinosa <joespla2000@gmail.com>
2018-04-21 15:08:09 -0500

    Sockets added

--- client.py
+++ client.py
@@ -1,9 +1,25 @@
 # This is the message sender
+import os
+from socket import *
 
 
-def algo():
+def keyGenerator():
     print("Hola")
 
 
 if __name__ == '__main__':
-    algo()
+
+    # Set the ip from the receiver
+    host = "0.0.0.0"
+    port = 13000
+    address = (host, port)
+    UDPSock = socket(AF_INET, SOCK_DGRAM)
+
+    while True:
+        data = input("Enter message to sent or type 'exit': ")
+        UDPSock.sendto(data, address)
+        if data == "exit":
+            break
+
+    UDPSock.close()
+    os._exit(0)
--- server.py
+++ server.py
@@ -8,16 +8,15 @@ port = 13000
 buf = 1024
 address = (host, port)
 UDPSock = socket(AF_INET, SOCK_DGRAM)
+
 UDPSock.bind(address)
 print("Receiving messages. Please wait...")
+
 while True:
     (data, address) = UDPSock.recvfrom(buf)
     print("Received message: " + data)
     if data == "exit":
         break
+
 UDPSock.close()
 os._exit(0)
-
-
-
-
